62283efaac9b9665bf8d77e54cf942144d7096eb
arjalasya <88618274+arjalasya@users.noreply.github.com>
2021-08-08 17:56:28 +0530

    Create Proj1.py

---
+++ Proj1.py
@@ -0,0 +1,12 @@
+import math;
+r = float(input("Input the radius of the circle : "))
+print("Input the radius of the circle :",r,"The area of the circle with radius ",r,"is:",(math.pi*r*r))
+#print("Input the radius of the circle : %.1f"%r,"The area of the circle with radius  %.1f"%r,"is: %.16f "%(math.pi*r*r))
+
+
+n = str(input())
+ext = n.split(".")
+if ext[1]=="py":
+    print("Input the Filename:",n,"The extension of the file is:'python'")
+else:
+    print("Input the Filename:",n,"The extension of the file is:'%s'"%ext[1])

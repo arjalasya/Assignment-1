31a9219ea54411c9757c21a8d869f79adbbabfa9
arjalasya <88618274+arjalasya@users.noreply.github.com>
2021-08-08 18:44:37 +0530

    Update Proj1.py

--- Proj1.py
+++ Proj1.py
@@ -1,7 +1,8 @@
 import math;
 r = float(input("Input the radius of the circle : "))
 print("Input the radius of the circle :",r,"The area of the circle with radius ",r,"is:",(math.pi*r*r))
-#print("Input the radius of the circle : %.1f"%r,"The area of the circle with radius  %.1f"%r,"is: %.16f "%(math.pi*r*r))
+#print("Input the radius of the circle : %.1f"%r,"The area of the circle with radius  %.1f"%r,"is: %.16f "%(math.pi*r*r))  
+
 
 
 n = str(input())
@@ -10,3 +11,15 @@ if ext[1]=="py":
     print("Input the Filename:",n,"The extension of the file is:'python'")
 else:
     print("Input the Filename:",n,"The extension of the file is:'%s'"%ext[1])
+
+   
+'''
+dict1 = {"py":"Python",
+        "c":"C",
+        "java":"java",
+        "html":"html"
+        }
+file=str(input())
+ext1 = file.split(".")
+print("Input the Filename:",file,"The extension of the file is:{}",format(dict.get(ext1[1]))
+'''
